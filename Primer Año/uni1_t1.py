b38391f8830d00641264640341e03a95cc7e08b0
Jonathan <124144015+joninet@users.noreply.github.com>
2023-08-01 20:06:39 -0300

    ok

--- Primer Año/uni1_t1.py
+++
@@ -1,5 +0,0 @@
-# Pedir dos números enteros, sumarlos y mostrar el resultado.
-n1 = int(input("Ingrese su primer numero: "))
-n2 = int(input("ingrese su segundo numero: "))
-resultado = n1 + n2
-print("el resultado de la suma es", resultado)
