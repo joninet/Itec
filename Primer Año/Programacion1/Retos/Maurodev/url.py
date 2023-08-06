7bfd99ad96e724000aa03a7d29c248ad434b7641
joninet <joninet@msn.com>
2023-08-06 14:10:50 -0300

    o

---
+++ Primer Año/Programacion1/Retos/Maurodev/url.py
@@ -0,0 +1,21 @@
+"""/*
+ * Dada una URL con parámetros, crea una función que obtenga sus valores.
+ * No se pueden usar operaciones del lenguaje que realicen esta tarea directamente.
+ *
+ * Ejemplo: En la url https://retosdeprogramacion.com?year=2023&challenge=0
+ * los parámetros serían ["2023", "0"]
+ */"""
+pagina="https://retosdeprogramacion.com?year=2023&challenge=0"
+buscamos=pagina.find("=")
+num="0123456789"
+sumamos=""
+lista=[]
+while buscamos != -1:
+    for x in range(buscamos,len(pagina)):
+        letra=pagina[x]
+        if letra in num:
+            sumamos=sumamos + letra
+    lista.append(sumamos)
+buscamos=-1
+print(lista)
+
