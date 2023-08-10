ec339c18bed50b0f21b99ea094d3a98e436ab745
joninet <joninet@msn.com>
2023-08-10 20:37:32 -0300

    ok

--- Primer Año/Programacion1/Ejercicios_practica/repite.py
+++
@@ -1,9 +0,0 @@
-frase = "Quiero comer manzanas, solamente manzanas.manzanas"
-buscar = "manzanas"
-find = frase.find(buscar)
-contador = 0
-for rec in frase:
-    if find != -1:
-        contador += 1
-        find = frase.find(buscar, find+1)
-print("la palabra se repite ", contador)
