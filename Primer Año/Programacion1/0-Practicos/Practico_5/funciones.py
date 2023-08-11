3e38955d45c6c0428ed430777b7b2d74083e3577
Jonathan <124144015+joninet@users.noreply.github.com>
2023-08-11 12:51:29 +0000

    ok

--- Primer Año/Programacion1/0-Practicos/Practico_5/funciones.py
+++ Primer Año/Programacion1/0-Practicos/Practico_5/funciones.py
@@ -16,14 +16,20 @@ def fechaNacimiento(lista):
         letraIni=input("Ingresar Inicial a buscar: ")
         contador=0
         if len(letraIni) == 1:
+            listaFechas=[]
             for x in lista:
                 inicialLista=x.split(",")[0][0]
                 fecha=x.split(",")[2]
                 if letraIni == inicialLista:
                     contador+=1
-                    print(fecha)
+                    listaFechas.append(fecha)
+                validado=True
             if contador == 0:
                 print(f"No se encontraron nombres con la inicial {letraIni}")
+            else:
+                print(f"Las fechas de nacimiento con la inicial {letraIni} son:")
+                for x in listaFechas:
+                    print(x)
         else:
             print("se debe ingresar solo un caracter")
     return 
