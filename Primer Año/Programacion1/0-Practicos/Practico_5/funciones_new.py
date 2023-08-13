ce60650b26821b9568d9b60063d8ed6b0311a487
joninet <joninet@msn.com>
2023-08-13 19:37:20 -0300

    ok

---
+++ Primer Año/Programacion1/0-Practicos/Practico_5/funciones_new.py
@@ -0,0 +1,27 @@
+def contar_ocurrencias_pais(lista_personas, pais_buscar):
+    contador = 0
+    for persona in lista_personas:
+        pais = persona.split(",")[1]
+        if pais == pais_buscar:
+            contador += 1
+    
+    if contador > 0:
+        resultado = f"El país {pais_buscar} tiene {contador} personas."
+    else:
+        resultado = f"No se encontró el país {pais_buscar}."
+    
+    return resultado
+
+def fechas_nacimiento_con_inicial(lista_personas, inicial):
+    fechas_nacimiento = []
+    for persona in lista_personas:
+        inicial_lista = persona.split(",")[0][0]
+        if inicial == inicial_lista:
+            fechas_nacimiento.append(persona.split(",")[2])
+    
+    if not fechas_nacimiento:
+        resultado = f"No se encontraron nombres con la inicial {inicial}."
+    else:
+        resultado = f"Fechas de nacimiento con la inicial {inicial}:\n{', '.join(fechas_nacimiento)}"
+    
+    return resultado
---
+++ Primer Año/Programacion1/0-Practicos/Practico_5/main_new.py
@@ -0,0 +1,32 @@
+from funciones_new import *
+
+personas = [
+    "Vikki Tewkesbury,France,30-01-2000",
+    "Virgie Brach,France,04-02-1994",
+    "Adeline LaPadula,France,18-06-2002",
+    "Willy Branscombe,Argentina,21-11-1997",
+    "Diane Piffe,France,07-08-1993",
+    "Britta Causbey,France,24-04-1991",
+    "Elisabeth Cleeve,France,04-03-1991",
+    "Rafael Ivanchenkov,France,28-04-2002",
+    "Zerk Milsom,Norway,03-12-1994",
+    "Adorne Ovington,United States,17-08-1991",
+    "Kathryn Backshell,United States,04-03-1992",
+    "Blake Colbeck,United States,18-01-1999",
+    "Arron Bresnahan,United States,03-07-2001",
+    "Deloria Dominguez,France,31-07-1990",
+    "Grenville Aldersea,Argentina,11-01-2001",
+    "Jemimah Haughian,Argentina,30-11-1998",
+    "Con Gethen,United States,06-06-1992",
+    "Roxie Igoe,France,31-03-2002",
+    "Hollyanne Mottley,United States,05-01-1996",
+    "Ambrosio Cadore,Norway,09-12-2002"
+]
+
+print(contar_ocurrencias_pais(personas, "Argentina"))
+print("Opciones de países: Alemania - Estados Unidos - Noruega - Francia")
+pais_busqueda = input("Ingresa otro país además de Argentina: ")
+print(contar_ocurrencias_pais(personas, pais_busqueda))
+
+inicial_busqueda = input("Ingresa una inicial para buscar fechas de nacimiento: ")
+print(fechas_nacimiento_con_inicial(personas, inicial_busqueda))
