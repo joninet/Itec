b4538fb435451d625394810a3f09d3c6a1d42a16
Jonathan <124144015+joninet@users.noreply.github.com>
2023-08-17 12:14:50 +0000

    OK

--- Primer Año/Programacion1/9-Poo/Guia_7/ejer_4.py
+++ Primer Año/Programacion1/9-Poo/Guia_7/ejer_4.py
@@ -1,5 +1,16 @@
 # Definir una clase que al ser instanciada reciba un valor numérico y cargue una lista de nombres hasta esa cantidad. 
 # Hacer también un método que muestre la lista completa
 class Nombres:
-    def __init__(self):
+    def __init__(self,nombre):
+        self.nombre=nombre
+listaNombres=[]
+cantidadNombres=int(input("Cantidad de nombres a ingresar: "))
+for x in range(cantidadNombres):
+    n=input(f"ingresar el Nombre N°{x+1}: ")
+    agregoN=Nombres(n)
+    listaNombres.append(agregoN)
+print("Lista completa de nombres:")
+for i in listaNombres:
+    print(i.nombre)
+
         
