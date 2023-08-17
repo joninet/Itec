3c520fe7f8b5bef11b046667405fa04daad23a03
Jonathan <124144015+joninet@users.noreply.github.com>
2023-08-17 13:42:42 +0000

    ok

--- Primer Año/Programacion1/9-Poo/Guia_7/ejer_5.py
+++ Primer Año/Programacion1/9-Poo/Guia_7/ejer_5.py
@@ -1,3 +1,23 @@
 # Hacer una clase Persona con dos métodos: uno para saber si es mayor de edad y el otro para determinar si es varón o 
 # mujer. En el programa principal instanciarlo, tomar nombre, edad y sexo, y finalmente mostrar un cartel que diga por 
 # ejemplo ‘Juan es mayor de edad y es varón’.
+class Persona:
+    def __init__(self,nombre,edad,sexo):
+        self.nombre=nombre
+        self.edad=edad
+        self.sexo=sexo
+    def mayorEdad(self):
+        if self.edad >= 18:
+            resultado=f"{self.nombre} es mayor de edad"
+        else:
+            resultado=f"{self.nombre} es menor de edad"
+        return resultado
+    def verificarSexo(self):
+        if self.sexo == "M":
+            resultado="Varon"
+        else:
+            resultado="Mujer"
+        return resultado
+personaIngreso=Persona("Jonathan",19,"M")
+print(personaIngreso.mayorEdad)
+
