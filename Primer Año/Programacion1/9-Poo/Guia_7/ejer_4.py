d1a08e6d1a7aaf0ec73bae1698915d7231a8c760
Jonathan <124144015+joninet@users.noreply.github.com>
2023-08-17 12:19:51 +0000

    ok

--- Primer Año/Programacion1/9-Poo/Guia_7/ejer_4.py
+++ Primer Año/Programacion1/9-Poo/Guia_7/ejer_4.py
@@ -1,16 +1,36 @@
 # Definir una clase que al ser instanciada reciba un valor numérico y cargue una lista de nombres hasta esa cantidad. 
 # Hacer también un método que muestre la lista completa
-class Nombres:
-    def __init__(self,nombre):
-        self.nombre=nombre
-listaNombres=[]
-cantidadNombres=int(input("Cantidad de nombres a ingresar: "))
-for x in range(cantidadNombres):
-    n=input(f"ingresar el Nombre N°{x+1}: ")
-    agregoN=Nombres(n)
-    listaNombres.append(agregoN)
-print("Lista completa de nombres:")
-for i in listaNombres:
-    print(i.nombre)
+class ListaNombres:
+    def __init__(self, cantidad):
+        self.cantidad = cantidad
+        self.nombres = []
+
+    def cargar_nombres(self):
+        for i in range(self.cantidad):
+            nombre = input(f"Ingrese el nombre {i+1}: ")
+            self.nombres.append(nombre)
+for nombre in self.nombres:
+    print("")
+
+class ListaNombres:
+    def __init__(self, cantidad):
+        self.cantidad = cantidad
+        self.nombres = []
+
+    def cargar_nombres(self):
+        for i in range(self.cantidad):
+            nombre = input(f"Ingrese el nombre {i+1}: ")
+            self.nombres.append(nombre)
+
+    def mostrar_lista(self):
+        print("Lista de nombres:")
+        for nombre in self.nombres:
+            print(nombre)
+
+# Instanciar la clase y usar los métodos
+cantidad_nombres = int(input("Ingrese la cantidad de nombres a cargar: "))
+lista_nombres = ListaNombres(cantidad_nombres)
+lista_nombres.cargar_nombres()
+lista_nombres.mostrar_lista()
 
         
