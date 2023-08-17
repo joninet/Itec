bbe34681c4928fffd88a041e3ad4ab467618e951
Jonathan <124144015+joninet@users.noreply.github.com>
2023-08-17 13:24:34 +0000

    ok

--- Primer Año/Programacion1/9-Poo/Guia_7/ejer_4.py
+++ Primer Año/Programacion1/9-Poo/Guia_7/ejer_4.py
@@ -1,36 +1,19 @@
 # Definir una clase que al ser instanciada reciba un valor numérico y cargue una lista de nombres hasta esa cantidad. 
 # Hacer también un método que muestre la lista completa
-class ListaNombres:
-    def __init__(self, cantidad):
-        self.cantidad = cantidad
-        self.nombres = []
-
-    def cargar_nombres(self):
+class Nombres:
+    def __init__(self,cantidad):
+        self.cantidad=cantidad
+        self.lista=[]
+    def ingresoNombre(self):
         for i in range(self.cantidad):
-            nombre = input(f"Ingrese el nombre {i+1}: ")
-            self.nombres.append(nombre)
-for nombre in self.nombres:
-    print("")
+            nombre = input(f"Ingrese el nombre N°{i+1}: ")
+            self.lista.append(nombre)
+    def listaMostrar(self):
+        print("Lista completa:")
+        for rec in self.lista:
+            print(rec)
 
-class ListaNombres:
-    def __init__(self, cantidad):
-        self.cantidad = cantidad
-        self.nombres = []
-
-    def cargar_nombres(self):
-        for i in range(self.cantidad):
-            nombre = input(f"Ingrese el nombre {i+1}: ")
-            self.nombres.append(nombre)
-
-    def mostrar_lista(self):
-        print("Lista de nombres:")
-        for nombre in self.nombres:
-            print(nombre)
-
-# Instanciar la clase y usar los métodos
-cantidad_nombres = int(input("Ingrese la cantidad de nombres a cargar: "))
-lista_nombres = ListaNombres(cantidad_nombres)
-lista_nombres.cargar_nombres()
-lista_nombres.mostrar_lista()
-
-        
+cantidadIngresar = int(input("Ingrese la cantidad de nombres a cargar: "))
+listaN = Nombres(cantidadIngresar)
+listaN.ingresoNombre()
+listaN.listaMostrar()
---
+++ Primer Año/Programacion1/9-Poo/Guia_7/ejer_5.py
@@ -0,0 +1,3 @@
+# Hacer una clase Persona con dos métodos: uno para saber si es mayor de edad y el otro para determinar si es varón o 
+# mujer. En el programa principal instanciarlo, tomar nombre, edad y sexo, y finalmente mostrar un cartel que diga por 
+# ejemplo ‘Juan es mayor de edad y es varón’.
