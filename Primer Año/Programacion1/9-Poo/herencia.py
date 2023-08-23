836cee0c47123a89f62180f90743fee3c06bed3d
joninet <joninet@msn.com>
2023-08-23 11:37:43 -0300

    ok

---
+++ Primer Año/Programacion1/9-Poo/herencia.py
@@ -0,0 +1,11 @@
+class Animal:
+    def ponerNombre(self,n):
+        nombre.self=n
+class Gato(Animal):
+    pass
+class Perro(Animal):
+    pass
+nombreGato=Gato()
+nombreGato.ponerNombre("gtosss")
+print(nombreGato)
+#---------------
