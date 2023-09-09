798316f6b804c8afa10fa36ef63cda6e2a0a42a8
joninet <joninet@msn.com>
2023-09-08 22:30:01 -0300

    ok

--- Primer Año/Programacion1/0-Practicos/Practico_6/practico.py
+++ Primer Año/Programacion1/0-Practicos/Practico_6/practico.py
@@ -22,10 +22,63 @@ class Venado(Animal):
     def __init__(self,identificador,peso) -> None:
         super().__init__(identificador,peso)
     def estadoSalud(self):
-        if self.peso >= 200:
+        if self.peso >= 120:
             return f"Sano"
         else:
             return f"enfermo"
 
 class Jaula:
+    def __init__(self,tipoAnimal,carga):
+        self.tipoAnimal=tipoAnimal
+        self.carga=carga
+        self.listaAnimal=[]
+        for rec in range(len(carga)):
+            if tipoAnimal == "Puma":
+                peso,años=carga[rec]
+                identificador=rec+1
+                animal=Puma(identificador,peso,años)
+            elif tipoAnimal == "Venado":
+                peso=carga[rec]
+                identificador=rec+1
+                animal=Venado(identificador,peso)
+            self.listaAnimal.append(animal)
+    def obtenerAnimales(self):
+        datos=[]
+        for lista in self.listaAnimal:
+            salud = lista.estadoSalud()
+            datos.append(f"#{lista.identificador} - {salud}")
+        return datos
+    def cantidadAdultos(self):
+        datos=[]
+        for lista in self.listaAnimal:
+            años = lista.esAdulto()
+            datos.append(años)
+        return datos
+    
+            
+
+
+        
+
+
+cargaPumas = [(230, 6), (180, 4), (210, 7), (190, 10)]
+cargaVenados = [100, 130]
+
+jaulaPumas = Jaula("Puma", cargaPumas)
+jaulaVenados = Jaula("Venado", cargaVenados)
+
+for obtener in jaulaPumas.obtenerAnimales():
+    print(obtener)
+contador=0
+for contar in jaulaPumas.cantidadAdultos():
+    if contar == "Adulto":
+        contador+=1
+print(f"cantidad de adultos: {contador}")
+print("---")
+for obtener in jaulaVenados.obtenerAnimales():
+    print(obtener)
+
+
+
+
     
