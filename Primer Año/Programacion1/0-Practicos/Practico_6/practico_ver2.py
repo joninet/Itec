ef6fa2204b938cb7304f5441e9ab4c6d52f8ef82
joninet <joninet@msn.com>
2023-09-11 16:41:14 -0300

    ok

---
+++ Primer Año/Programacion1/0-Practicos/Practico_6/practico_ver2.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self,peso) -> None:
+        self.peso=peso
+    def estadoSalud(self):
+class Puma(Animal):
+    pass
+class Venado(Animal):
+    pass
+class Jaula(Animal):
+    pass
