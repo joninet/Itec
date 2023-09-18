02e4cb0ba27500b902a18b23221c61d08b001ad5
joninet <joninet@msn.com>
2023-09-17 21:23:16 -0300

    ok

--- Primer Año/Programacion1/pruebas.py
+++ Primer Año/Programacion1/pruebas.py
@@ -1,46 +1,74 @@
-import random
-print(hola)
-
-# Palabras a adivinar
-palabras = ['gato', 'perro', 'elefante', 'jirafa', 'leon', 'tigre', 'rinoceronte', 'hipopotamo']
-
-palabra_a_adivinar = random.choice(palabras)
-letras_ocultas = list(palabra_a_adivinar)
-num_letras_ocultas = int(len(letras_ocultas) * 0.6)
-indices_ocultos = random.sample(range(len(letras_ocultas)), num_letras_ocultas)
-for i in indices_ocultos:
-    letras_ocultas[i] = '_'
-palabra_oculta = ''.join(letras_ocultas)
-
-intentos_maximos = 5
-intentos_restantes = intentos_maximos
-
-print(f'¡Bienvenido al juego de adivinanza de palabras! La palabra a adivinar es: {palabra_oculta}')
-while intentos_restantes > 0:
-    print(f'Tienes {intentos_restantes} intentos restantes.')
-    respuesta = input('Ingresa una letra o una palabra: ').lower()
-    if len(respuesta) == 1:
-        if respuesta in palabra_a_adivinar:
-            print(f'¡Correcto! La letra {respuesta} está en la palabra.')
-            letras_ocultas = list(palabra_oculta)
-            for i, letra in enumerate(palabra_a_adivinar):
-                if letra == respuesta:
-                    letras_ocultas[i] = letra
-            palabra_oculta = ''.join(letras_ocultas)
-            print(f'La palabra a adivinar ahora es: {palabra_oculta}')
-            if palabra_oculta == palabra_a_adivinar:
-                print('¡Felicidades! Has adivinado la palabra.')
-                break
+from dataclasses import dataclass
+@dataclass
+class Asignatura:
+    nombre: str
+    nota: int
+    def setNota(self,notaNueva):
+        self.nota=notaNueva
+        return f"Se modifico la nota"
+    def getNota(self):
+        return self.nota
+    def Calificacion (self):
+        if self.nota >= 4:
+            return f"Aprobado"
         else:
-            print(f'Lo siento, la letra {respuesta} no está en la palabra.')
-            intentos_restantes -= 1
-    else:
-        if respuesta == palabra_a_adivinar:
-            print('¡Felicidades! Has adivinado la palabra.')
-            break
-        else:
-            print('Lo siento, esa no es la palabra a adivinar.')
-            intentos_restantes -= 1
-
-if intentos_restantes == 0:
-    print(f'Lo siento, has perdido. La palabra a adivinar era {palabra_a_adivinar}.')
+            return f"desaprobado"
+    def getNombre(self):
+        return self.nombre
+    
+@dataclass
+class Alumno:
+    nombre: str
+    edad: int
+    listaAsignaturas=[]
+    def setNombre(self,nombreNuevo):
+        self.nombre=nombreNuevo
+        return f"Se modifico el nombre"
+    def getNombre(self):
+        return self.nombre
+    def setEdad(self,edadNuevo):
+        self.edad=edadNuevo
+        return f"Se modifico la edad"
+    def getNombre(self):
+        return self.edad
+    def Promedio(self):
+        promedio=0
+        for recnota in self.listaAsignaturas:
+            promedio+=recnota.nota
+            return promedio / len(self.listaAsignaturas)
+    def agregarAsignatura(self):
+        validado=False
+        while not validado:
+            asigNueva=input("Ingresar asignatura nueva: ").lower()
+            if asigNueva.isalpha():
+                for asignatura in self.listaAsignaturas:
+                    if asigNueva == asignatura.nombre:
+                        return print(f"la asignatura ya existe")
+                validado=False
+                while not validado:
+                    try:
+                        notaNueva=int(input("Ingresar Nota: "))
+                        agregoAsig=Asignatura(asigNueva,notaNueva)
+                        self.listaAsignaturas.append(agregoAsig)
+                        return print(f"Agregada correctamente")
+                        validado=True
+                    except:
+                        print("Ingresar solo numeros")
+            else:
+                print("Ingresar solo letras")
+    def mostrarTodo(self):
+        print(f"Nombre Alumno: {self.nombre}")
+        print(f"Edad: {self.edad}")
+        print("Asignaturas que curso:")
+        for asignatura in self.listaAsignaturas:
+            print(f"Nombre: {asignatura.nombre}")
+            print(f"Nota: {asignatura.nota}")
+            print(f"{asignatura.Calificacion()}")
+    
+alumno1=Alumno("jonathan",35)
+alumno1.agregarAsignatura()
+alumno1.agregarAsignatura()
+alumno1.agregarAsignatura()
+alumno1.mostrarTodo()
+alumno1.Promedio()
+    
