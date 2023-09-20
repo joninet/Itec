5151ab19d0271be84fe45528b6433bf8ce9434d2
joninet <joninet@msn.com>
2023-09-20 20:53:56 -0300

    ok

---
+++ Primer Año/Programacion1/0-Practicos/Parciales/parcial2.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+@dataclass
+class Video:
+    nombre: str
+    visualizaciones: int
+    actores: str
+
+@dataclass
+class Series(Video):
+    temporadas: int
+
+@dataclass
+class Peliculas(Video):
+    minutos: int
+
+@dataclass
+class Netflix:
+    listaSeries=[]
+    listaPeliculas=[]
+    series = [["Peaky Blinders", 1234567, 'Cillian Murphy,Paul Anderson,Helen McCrory', 6], ["The Umbrella Academy", 2434908, 'Tom Hopper,Emmy Raver-Lampman,Ellen Page,David Castañeda', 2]]
+    pelis = [["Inmortales", 35, 'Mirtha Legrand,Carlitos Balá,Elizabeth The Second', 30],["Inception", 17319533, 'Leonardo DiCaprio,Ellen Page,Joseph Gordon-Levitt', 148], ["Batman Begins",4760183, 'Christian Bale,Leonardo DiCaprio,Cillian Murphy,Michael Caine', 140]]
+    for x in series:
+        nombre,visu,actores,temp=x
+        serie=Series(nombre,visu,actores,temp)
+        listaSeries.append(serie)
+    for x in pelis:
+        nombre,visu,actores,min=x
+        pelicula=Peliculas(nombre,visu,actores,min)
+        listaPeliculas.append(pelicula)
+    def mostrar(self):
+        for x in self.listaPeliculas:
+            print(x.nombre)
+    def videoMasVisto(self):
+        contador=0
+        pelicula=""
+        for rec in self.listaPeliculas:
+            if rec.visualizaciones > contador:
+                contador=rec.visualizaciones
+                pelicula=rec.nombre
+        return print(f"El video mas visto: {pelicula}")
+    def seriesMenosdetres(self):
+        print("Series que tienen menos de 3 temporadas:")
+        for rec in self.listaSeries:
+            if rec.temporadas < 3:
+                print(rec.nombre)
+    def mismosActores(self):
+        actoresSeries = []
+        actoresPeliculas = []
+        print("Actores que trabajan en series y películas:  ")
+        for x in self.listaPeliculas:
+            actoresP = x.actores.split(",")
+            for i in actoresP:
+                actoresPeliculas.append(i)
+        for x in self.listaSeries:
+            actoresS = x.actores.split(",")
+            for i in actoresS:
+                actoresSeries.append(i)
+        for x in actoresPeliculas:
+            if x in actoresSeries:
+                print(x)
+
+            
+
+        
+a=Netflix()
+a.videoMasVisto()
+print()
+a.seriesMenosdetres()
+print()
+a.mismosActores()
+
+
---
+++ Primer Año/Programacion1/0-Practicos/Parciales/probando.py
@@ -0,0 +1,2 @@
+lista=["Hola", "mundo", "mundo"]
+print(lista.count("mundo"))
