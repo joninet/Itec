6364abcfdc71994303bba93358febef06f057a22
Jonathan <124144015+joninet@users.noreply.github.com>
2023-09-20 18:19:07 +0000

    ok

--- Primer Año/Programacion1/0-Practicos/antes_del_parcial.py/videos.py
+++ Primer Año/Programacion1/0-Practicos/antes_del_parcial.py/videos.py
@@ -15,3 +15,7 @@ class Peliculas(Video):
 
 @dataclass
 class appVideo:
+    self.listaSeries=[]
+    self.listaPeliculas=[]
+    self.series = [["Peaky Blinders", 1234567, 'Cillian Murphy, Paul Anderson, Helen McCrory', 5],["The Umbrella Academy", 2434908, 'Tom Hopper, Emmy Raver-Lampman, Ellen Page, David Castañeda', 2]]
+    self.pelis = [["Inception", 4760183, 'Leonardo DiCaprio, Ellen Page, Joseph Gordon-Levitt', 148],["Batman Begins", 17319533, 'Christian Bale, Cillian Murphy, Michael Caine', 140],["Inmortales", 35, 'Mirtha Legrand, Leonardo DiCaprio, Elizabeth The Second', 30]]
