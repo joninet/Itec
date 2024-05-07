14a41d32798b8c91f03e8f085592e4e0659e3c14
Jonathan Desplats <124144015+joninet@users.noreply.github.com>
2024-05-07 09:21:08 +0000

    ok

---
+++ Segundo_Ano/PP_Python/Segundo_Proyecto/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/') # app es la instancia, route el metodo, '/' es el disparador
+def index():
+    return render_template(
+        'index.html',
+    )
+
+@app.route('/info') # app es la instancia, route el metodo, '/' es el disparador
+def informacion():
+    return render_template(
+        'informacion.html',
+    )
+
+@app.route('/bienvenido/<nombre>')
+def bienvenido(nombre):
+    return render_template('bienvenida.html', nombre=nombre)
